347affbcebbde8273b7da5417231494dce74b3e2
Tomáš Prokeš <prokest17@gmail.com>
2024-11-14 10:36:57 +0100

    morning flash disabled

--- resources/js/projects.js
+++ resources/js/projects.js
@@ -46,7 +46,7 @@ document.addEventListener("DOMContentLoaded", function() {
     const time = new Date();
     const hours = time.getHours();
     
-    if(hours >= 19){
+    if(hours >= 19 && hours <= 8){
         document.body.style.backgroundImage = 'linear-gradient(to right, #2a1a4c, #2d3f71)';
         projectContainer.forEach((projectContainer) => {
             projectContainer.style.backgroundColor = '#2d3f71';
