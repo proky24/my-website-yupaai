2018e9c3895d2eb6e4dea8064c9014f5d2f587ad
PirateCZ <fschod@seznam.cz>
2024-12-02 09:08:52 +0100

    little clean up

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,13 +1,11 @@
 const body = document.getElementById('body');
 const socials = document.getElementById('socials');
 const projectsBtn = document.getElementById('projectsBtn');
-projectsBtn.addEventListener('click', () => {
-    window.location.href = '/projects.html';
-});
-
 const aboutBtn = document.getElementById('aboutMeBtn');
+const aboutMe = document.querySelector('.about-me');
 
-let observer1 = new IntersectionObserver((entries, observer1) => {
+
+let aboutMeBtnObserver = new IntersectionObserver((entries, observer1) => {
     entries.forEach(entry => {
         if(entry.isIntersecting){
             requestAnimationFrame(() => {
@@ -22,10 +20,9 @@ let observer1 = new IntersectionObserver((entries, observer1) => {
         }
     });
 });
+aboutMeBtnObserver.observe(aboutBtn);
 
-observer1.observe(aboutBtn);
-
-let observer2 = new IntersectionObserver((entries, observer2) => {
+let projectsObserver = new IntersectionObserver((entries, observer2) => {
     entries.forEach(entry => {
         if(entry.isIntersecting){
             requestAnimationFrame(() => {
@@ -40,11 +37,9 @@ let observer2 = new IntersectionObserver((entries, observer2) => {
         }
     });
 });
+projectsObserver.observe(projectsBtn);
 
-observer2.observe(projectsBtn);
-
-
-let observer3 = new IntersectionObserver((entries, observer3) => {
+let socialsObserver = new IntersectionObserver((entries, observer3) => {
     entries.forEach(entry => {
         if(entry.isIntersecting){
             requestAnimationFrame(() => {
@@ -59,69 +54,47 @@ let observer3 = new IntersectionObserver((entries, observer3) => {
         }
     });
 });
+socialsObserver.observe(socials);
 
-observer3.observe(socials);
-
-
-const aboutMe = document.querySelector('.about-me');
-    // Define the Intersection Observer
-    let observer = new IntersectionObserver((entries, observer) => {
-        entries.forEach(entry => {
-            // If the element is in the viewport, add the animation class
-            if(entry.isIntersecting) {
-                requestAnimationFrame(() => {
-                    entry.target.style.opacity = '1';
-                    entry.target.classList.add('addSlideIn');
-                    setTimeout(() => {
-                        entry.target.classList.remove('addSlideIn');
-                        /*entry.target.style.marginBottom = '12px';
-                        entry.target.style.marginTop = '400px';*/
-                    }, 1500);
-                })
-            }else{
-                entry.target.style.opacity = '0';
-            }
-        });
-    }, {threshold: 0.6}); // Adjust the threshold according to your needs
-
-    // Start observing the aboutMe element
-    observer.observe(aboutMe);
+let aboutMeObserver = new IntersectionObserver((entries, observer) => {
+    entries.forEach(entry => {
+        if(entry.isIntersecting) {
+            requestAnimationFrame(() => {
+                entry.target.style.opacity = '1';
+                entry.target.classList.add('addSlideIn');
+                setTimeout(() => {
+                    entry.target.classList.remove('addSlideIn');
+                }, 1500);
+            })
+        }else{
+            entry.target.style.opacity = '0';
+        }
+    });
+}, {threshold: 0.6});
+aboutMeObserver.observe(aboutMe);
 
 document.addEventListener("DOMContentLoaded", function() {
-    /*const hey = document.querySelector('.hey');
-    const name = document.querySelector('.name');
-    const about = document.querySelector('.about');
-    const studying = document.querySelector('.studying');
-    */
+
    const div = document.querySelector('.container-main');
    const pic = document.querySelector('.pic');
-   
-   function addOpacity(){
-       /*hey.classList.add('addOpacity');
-        name.classList.add('addOpacityName');
-        pic.classList.add('addOpacityPic');
-        about.classList.add('addOpacityAbout');
-        studying.classList.add('addOpacityStudying');
-        */
-       div.classList.add('addOpacity');
-       pic.classList.add('addShadowColor');
-    }
-    
-    addOpacity();
-});
 
+    div.classList.add('addOpacity');
+    pic.classList.add('addShadowColor');
+    
+});
     
 
 function scrollDown(){
     aboutMe.style.opacity = '1';
     aboutMe.classList.add('addSlideIn');
     aboutMe.scrollIntoView({behavior: "smooth"});
-    setTimeout(() => {
-        /*aboutMe.classList.remove('addSlideIn');
-        /*aboutMe.style.marginTop = '12px';*/
-    }, 1500);
 }
 
+function redirectToProjects(){
+    window.location.assign("/projects.html");
+}
+
+//this creates a dark version of thes site
 document.addEventListener("DOMContentLoaded", function() {
     const time = new Date();
     const hours = time.getHours();
